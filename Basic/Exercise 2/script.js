b9cf1b96f6f492104013fe364f96cbc338329689
Ervin Ademovic <newea1984@gmail.com>
2020-05-28 10:27:58 +0200

    Exercise one finished

---
+++ Basic/Exercise 2/script.js
@@ -0,0 +1,16 @@
+const name = document.getElementById("name").value;
+const lastname = document.getElementById("lastname").value;
+const age = document.getElementById("age").value;
+const name_out = document.querySelector(".name_out");
+const lastname_out = document.querySelector(".lastname_out");
+const age_out = document.querySelector(".age_out");
+
+let charLength = 5;
+
+if (name.length > charLength || lastname.length > charLength) {
+    name_out.style.color = "green";
+    name_out.innerHTML = name;   
+} else {
+    name_out.style.color = "red";
+    name_out.innerHTML = name;
+}
