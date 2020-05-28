6d9910065c1cce5adecef2b88c87aadafd3a6fcb
Ervin Ademovic <newea1984@gmail.com>
2020-05-28 11:43:19 +0200

    Exercise 2 finished

--- Basic/Exercise 2/script.js
+++ Basic/Exercise 2/script.js
@@ -1,19 +1,17 @@
 const name = document.getElementById("name").value;
 const lastname = document.getElementById("lastname").value;
 const age = document.getElementById("age").value;
-const it = document.getElementsByClassName("it")[0];
-const hos = document.getElementsByClassName("hos")[1];
-const bro = document.getElementsByClassName("bro")[2];
+const it = document.querySelector(".it").value;
+const hos = document.querySelector(".hos").value;
+const bro = document.querySelector(".bro").value;
+const sell = document.querySelector(".sell").value;
+const prof = document.querySelector(".profession").value;
 
 
 const name_out = document.querySelector(".name_out");
 const lastname_out = document.querySelector(".lastname_out");
 const age_out = document.querySelector(".age_out");
 const prof_out = document.querySelector(".prof_out");
-
-//prof_out.innerHTML="test";
-//console.log("it")
-
 
 let charLength = 5;
 
@@ -25,21 +23,24 @@ if (name.length > charLength || lastname.length > charLength) {
     name_out.innerHTML = name;
 }
 
-console.log(it)
-console.log(hos)
-console.log(bro)
 
-if (it) {
+switch (prof) {
+    case "IT":
     prof_out.style.color = "purple";
-    prof_out.innerHTML = it.value;//code
-    
-}
-else if (hos) {
-    prof.out.style.color = "yellow"
-    prof_out.innerHTML = hos.value;
-    //code
-}
-else{
-    prof.out.style.color = "pink"
-    prof_out.innerHTML = bro.value;
+    prof_out.innerHTML = it;
+    break;
+    case "Hospitality":
+    prof_out.style.color = "yellow"
+    prof_out.innerHTML = hos;
+    break;
+    case "Broker":
+    prof_out.style.color = "pink"
+    prof_out.innerHTML = bro;
+    break;
+    case "Seller":
+    prof_out.style.color = "blue"
+    prof_out.innerHTML = sell;
+    break;
+    default:
+    prof_out.innerHTML = "UPPSS - An error has occured";  
 }
